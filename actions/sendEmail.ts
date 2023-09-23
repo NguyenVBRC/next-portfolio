5c97b97ea8f3a71a6eeedbaf328d19febcd67d4d
Jimmy Nguyen <jimmy@gcompartners.com>
2023-09-22 22:38:04 -0500

    Updated Env Variables

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,4 +1,4 @@
-  "use server";
+"use server";
 
 import React from "react";
 import { Resend } from "resend";
@@ -26,7 +26,7 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
+      from: "Contact Form <JimmyNguyenPortfolio@resend.dev>",
       to: "nguyen89.mke@gmail.com",
       subject: "Message from Portfolio Contact Form",
       reply_to: senderEmail,
