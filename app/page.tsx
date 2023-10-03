1c7af50ebd78f2956feeadc473456690e5aec4ff
nguyen89mke <nguyen89.mke@gmail.com>
2023-10-03 05:00:59 -0500

    Business Card Waterfall

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,16 @@
-import Intro from "@/components/Introduction";
-import About from "@/components/About";
-import Contact from "@/components/Contact";
-import Skills from "@/components/Skills";
-import Experience from "@/components/Experience";
-import Projects from "@/components/Projects";
+import BusinessCard from "@/components/content/businessCard/BusinessCard";
+// import Technologies from "@/components/content/Technologies";
+// import Experience from "@/components/content/Experience";
 import SectionDivider from "@/components/SectionDivider";
 import ResourceBtns from "@/components/ResourceBtns";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
-      <Intro />
-      <ResourceBtns />
-      <SectionDivider />
-      <About />
-      {/* <Projects /> */}
-      <Skills />
-      <Experience />
-      <Contact />
+      <BusinessCard />
+      {/* <SectionDivider />
+      <Technologies />
+      <Experience /> */}
     </main>
   );
 }
---
+++ components/content/Contact.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "../SectionHeading";
+import { motion } from "framer-motion";
+import { sendEmail } from "@/actions/sendEmail";
+import SubmitBtn from "../buttons/SubmitBtn";
+import toast from "react-hot-toast";
+
+export default function Contact() {
+  return (
+    <motion.section
+      className="fixed bottom-[6rem] w-[90%] text-center border-[0.35rem] border-gray-1000 shadow-xl rounded-xl p-[1rem] bg-gray-100 dark:bg-gray-950 dark:border-black/40 dark:border-white sm:bottom-5 sm:right-[11rem] sm:w-[25rem] sm:bottom-5"
+      transition={{
+        duration: 0.5,
+        type: "spring",
+        stiffness: 70,
+      }}
+      initial={{ y: 100, opacity: 0.5 }}
+      animate={{ y: 0, opacity: 1 }}
+    >
+      <SectionHeading>Contact me</SectionHeading>
+      <form
+        className="flex flex-col dark:text-black"
+        action={async (formData) => {
+          const { data, error } = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success("Email sent successfully!");
+        }}
+      >
+        <input
+          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          name="senderEmail"
+          type="email"
+          required
+          maxLength={500}
+          placeholder="Your Email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          name="message"
+          placeholder="Your Message"
+          required
+          maxLength={5000}
+        />
+        <SubmitBtn />
+      </form>
+    </motion.section>
+  );
+}
