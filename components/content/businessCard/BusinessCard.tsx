7f5672d0ec47ff604a04c5de768f2b7cfdde5a28
nguyen89mke <nguyen89.mke@gmail.com>
2023-10-09 20:41:04 -0500

    BusinesCard Comp: Removed Object-Contain

--- components/content/businessCard/BusinessCard.tsx
+++ components/content/businessCard/BusinessCard.tsx
@@ -30,7 +30,7 @@ export default function Intro() {
         animate={controls}
         transition={{ duration: 0.5 }}
       >
-        <div className="rounded-xl object-cover border-[0.35rem] border-gray-900 shadow-xl p-7 relative dark:border-teal-50">
+        <div className="rounded-xl border-[0.35rem] border-gray-900 shadow-xl p-7 relative dark:border-teal-50">
           {!isFlipped ? <FrontCard /> : <About />}
         </div>
       </motion.div>
