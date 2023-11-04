513b7240f810b327bf950b15b9d87a05992ad03c
nguyen89mke <nguyen89.mke@gmail.com>
2023-11-04 16:53:41 -0500

    Btns Smaller on Mobile

--- components/buttons/SocialsBtn.tsx
+++ components/buttons/SocialsBtn.tsx
@@ -17,18 +17,28 @@ const SocialsBtn = () => {
     setShowResumeDownload((prevShowResumeDownload) => !prevShowResumeDownload);
   };
 
-  const btnStyles = "h-[40px] w-[40px] border-[1px] border-slate-50 text-gray-50 flex justify-center items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] cursor-pointer";
+  const btnStyles =
+    "h-[30px] w-[30px] border-[1px] border-slate-50 text-gray-50 flex justify-center items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] cursor-pointer";
+  const mobileBtnStyles = "sm:h-[40px] sm:w-[40px]";
 
   return (
     <div className="relative">
       <motion.div className="fixed left-5 bottom-5 flex flex-col justify-center items-center gap-4">
-        <a className={btnStyles} onClick={handleClick}>
+        <a className={`${btnStyles} ${mobileBtnStyles}`} onClick={handleClick}>
           CV
         </a>
-        <a className={btnStyles} href="https://www.linkedin.com/in/jimmynguyendev/" target="_blank">
+        <a
+          className={`${btnStyles} ${mobileBtnStyles}`}
+          href="https://www.linkedin.com/in/jimmynguyendev/"
+          target="_blank"
+        >
           <BsLinkedin />
         </a>
-        <a className={btnStyles} href="https://github.com/NguyenVBRC" target="_blank">
+        <a
+          className={`${btnStyles} ${mobileBtnStyles}`}
+          href="https://github.com/NguyenVBRC"
+          target="_blank"
+        >
           <FaGithubSquare />
         </a>
         <div className="h-[40px] border-[1px] border-slate-50"></div>
